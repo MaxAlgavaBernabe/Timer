c448a33336e3610b5a57e58ffbfe29c687d27b18
MaxAlgavaBernabe <71739044+MaxAlgavaBernabe@users.noreply.github.com>
2020-09-22 19:33:58 -0500

    Update PROT-crono.ino

--- PROT-crono.ino
+++ PROT-crono.ino
@@ -1,6 +1,9 @@
+//Timer
+//@Max Algava
 
-#include <Wire.h>
 #include <LiquidCrystal.h>
+
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
 
 #include <Wire.h>
@@ -33,62 +36,62 @@ void setup(){
  
 
 void loop(){
-  //botao de início 
-  if(digitalRead(inicio) == HIGH){ //se o pino ler nível alto, zera as variáveis
+   
+  if(digitalRead(inicio) == HIGH){ 
     horas = 0;
     minutos = 0;
     segundos = 0;
     decimas = 0;
     lcd.clear();
   }
-  //botão Pause/Play 
-  if(digitalRead(pausa) == HIGH){//rotina de pausa e exibição da pausa
+  
+  if(digitalRead(pausa) == HIGH){
     delay(200);
     while(digitalRead(pausa) == LOW);
     lcd.setCursor(0,1);
-   if(horas < 10){ lcd.print("0");}//se as horas forem menor que 10, acrescenta um 0 na frente
+   if(horas < 10){ lcd.print("0");}
     lcd.print(horas);
     lcd.print(":");
-   if(minutos < 10){ lcd.print("0");}//se os minutos forem menor que 10, acrescenta um 0 na frente
+   if(minutos < 10){ lcd.print("0");}
     lcd.print(minutos);
     lcd.print(":");
-   if(segundos < 10){ lcd.print("0");}//se os segundos forem menor que 10, acrescenta um 0 na frente 
+   if(segundos < 10){ lcd.print("0");}
     lcd.print(segundos);
     lcd.print(":");
     lcd.print(decimas);
     delay(200);
   }
   milisegundos = millis();
-  if(milisegundos % 10 == 0){  //Entrar apenas se tiver passado o décimo de segundo
+  if(milisegundos % 10 == 0){  
     decimas++;
-    if(decimas == 100){ //Quando passou 10 décimos de segundo, conta um segundo
+    if(decimas == 100){ 
       decimas = 0;
       segundos++;
     }
     if(segundos == 60){ 
-// Após 60 segundos, conta um minuto
+
       segundos = 0;
       minutos++;
     }
-    if(minutos == 60){// Depois de 60 minutos, conta uma hora
+    if(minutos == 60){
       minutos = 0;
       horas++;
     }
-    //exibição no LCD
+  
     lcd.setCursor(0,0);
-   if(horas < 10){ //se as horas forem menor que 10, acrescenta um 0 na frente
+   if(horas < 10){ 
     lcd.print("0");
    }
     lcd.print(horas);
     lcd.print(":");
     lcd.setCursor(3,0);
-   if(minutos < 10){//se os minutos forem menor que 10, acrescenta um 0 na frente
+   if(minutos < 10){
      lcd.print("0");
     }
     lcd.print(minutos);
     lcd.print(":");
     lcd.setCursor(6,0);
-   if(segundos < 10){//se os segundos forem menor que 10, acrescenta um 0 na frente
+   if(segundos < 10){
      lcd.print("0");
     }
     lcd.print(segundos);
